427437e44e4e2745f4e233a64fb9063699835a1c
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-08-13 23:44:22 -0400

    Update

--- C++/List/ArrayList/main.cpp
+++ C++/List/ArrayList/main.cpp
@@ -10,12 +10,13 @@ using namespace std;
 
 int main(){
     //create an empty array of size 15 and type int
-    ArrayList<int> testArray(20);
+
+    List<int> *testArray = new ArrayList<int>(20);
     for (int i = 0; i < 20; i++){
-        testArray.insertLast(i);
+        testArray->insertLast(i);
     }
-    cout << testArray.get(7) << endl;
-    
+    cout << testArray->get(7) << endl;
+    delete testArray;
     return 1;
 
 }
